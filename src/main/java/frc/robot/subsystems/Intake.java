777c78286e95cff8091565b34aded7c45c979dae
Ryan Ruiz <ruiz.ryan.nj@gmail.com>
2026-01-12 14:05:25 -0500

    Replaced references to SparkMax to TalonFX.

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,116 @@
+// intake skeleton code
+
+package frc.robot.subsystems;
+
+/*
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.ResetMode;
+import com.revrobotics.PersistMode;
+*/
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase{
+    // Adjust motor IDs
+    private int kIntakeID = 35;
+    private int kPivotID = 36;
+
+
+    /* No longer using SparkMAX.
+    // SparkMAX configuration
+    // SparkMAX controls NEO 550. NOT compatible with CANivore.
+    private SparkMax intakeMotor;
+    private SparkMaxConfig intakeConfig;
+    */
+
+    // Kraken X60 that controls intake mechanism.
+    private TalonFX intakeMotor;
+    private TalonFXConfiguration intakeConfig;
+    private int kIntakeCurrentLimit = 15;
+
+    // Kraken X60 controls adjustment mechanism. Compatible with CANivore.
+    private TalonFX pivotMotor;
+    private TalonFXConfiguration pivotConfig;
+    private int kPivotCurrentLimit = 10;
+
+    public Intake() {
+        /* No longer using SparkMAX
+        
+        // Create SparkMAX object
+        this.intakeMotor = new SparkMax(kIntakeID, MotorType.kBrushless);
+
+        // Create SparkMAX Configs object, set current limit and idle mode
+        this.intakeConfig = new SparkMaxConfig();
+        intakeConfig
+            .smartCurrentLimit(kIntakeCurrentLimit)
+            .idleMode(IdleMode.kBrake);
+        
+        // Apply SparkMAX configurations: can choose to reset safe parameters or save parameters to controller
+        intakeMotor.configure(intakeConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+        */
+
+        // Create TalonFX object and configuration for intake motor
+        this.intakeMotor = new TalonFX(kIntakeID);
+        this.intakeConfig = new TalonFXConfiguration();
+
+        // Set current limit for the intake TalonFX motor controller
+        intakeConfig.CurrentLimits.SupplyCurrentLimit = kIntakeCurrentLimit;
+        intakeConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+        
+        intakeConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        intakeConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+
+        // Create TalonFX object, configurations
+        this.pivotMotor = new TalonFX(kPivotID);
+        this.pivotConfig = new TalonFXConfiguration();
+
+        // Set current limit for the pivot TalonFX motor controller
+        pivotConfig.CurrentLimits.SupplyCurrentLimit = kPivotCurrentLimit;
+        pivotConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+
+        // Set neutral mode and sensor source for Talon FX
+        pivotConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        pivotConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+
+        // Apply configurations to TalonFX
+        pivotMotor.getConfigurator().apply(pivotConfig);
+    }
+
+    // Stop both motors
+    public void stop() {
+        intakeMotor.stopMotor();
+        pivotMotor.stopMotor();
+    }
+
+
+    // Intake pivot
+    public void setVoltagePivot(double voltage) {
+        this.pivotMotor.setVoltage(voltage);
+    }
+
+    public Command adjustIntakePivot(double voltage) { 
+        return this.runEnd(() -> setVoltagePivot(voltage), () -> stop());
+    }
+    // END intake pivot
+
+    // Fuel Intake 
+    public void setVoltageFuelIntake(double voltage) {
+        intakeMotor.setVoltage(voltage);
+    }
+
+    public Command intakeFuelPiece(double voltage) {
+        return this.runEnd(() -> setVoltageFuelIntake(voltage), () -> stop());
+    }
+    // END fuel intake
+
+
+}
