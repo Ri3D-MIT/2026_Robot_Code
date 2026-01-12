622f38930fa907975475a4ffcee95ed2d30316b0
sigalrmp <sigalrmp@gmail.com>
2026-01-12 16:53:17 -0500

    some control, no constants

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -11,7 +11,6 @@ public final class Ports {
           entry(Shooter.BOTTOM_FOLLOWER, "bottom shooter"));
 
   public static final class Shooter {
-    // TODO make sure leader/follower is accurate
     public static final int TOP_LEADER = 31;
     public static final int BOTTOM_FOLLOWER = 32;
   }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems;
 
 import static frc.robot.Ports.Shooter.*;
 
+import java.util.function.DoubleSupplier;
+
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 // import com.ctre.phoenix6.CANBus;
@@ -9,6 +11,9 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.FaultLogger;
@@ -18,6 +23,24 @@ public class Shooter extends SubsystemBase {
 
   private final TalonFX topMotor;
   private final TalonFX bottomMotor;
+
+  private final double kS = 0;
+  private final double kV = 0;
+  private final double kA = 0;
+
+  private final double kP = 0;
+  private final double kD = 0;
+
+  private final SimpleMotorFeedforward ff = new SimpleMotorFeedforward(kS, kV, kA);
+  private final PIDController fb = new PIDController(kP, 0, kD);
+
+  // TODO actually set constants
+
+  /** pid tolerance, radians per second */
+  private final double TOLERANCE = 10;
+
+  /** max flywheel speed, rads per sec */
+  private final double MAX_SPEED = 100;
 
   public Shooter() {
     this.topMotor = new TalonFX(TOP_LEADER);
@@ -38,6 +61,8 @@ public class Shooter extends SubsystemBase {
 
     FaultLogger.register(topMotor);
     FaultLogger.register(bottomMotor);
+
+    fb.setTolerance(TOLERANCE);
   }
 
   public void stopMotors() {
@@ -47,18 +72,23 @@ public class Shooter extends SubsystemBase {
 
   public void setShooterVoltage(double voltage) {
     topMotor.setVoltage(voltage);
-    // lowerMotor.setVoltage(-voltage);
   }
 
-  public void setVoltageRight(double voltage) {
-    // lowerMotor.setVoltage(voltage);
+  /** updates velocity goal, radians per second */
+  public void updateGoal(double velocity) {
+    double goal = Double.isNaN(velocity) ? 0 : MathUtil.clamp(velocity, -MAX_SPEED, MAX_SPEED);
+    // TODO make sure units check out
+    double current_velocity = topMotor.getVelocity().getValueAsDouble();
+
+    setShooterVoltage(
+        fb.calculate(current_velocity) + ff.calculateWithVelocities(current_velocity, goal));
   }
 
-  public Command runShooter(double voltage) {
-    return this.runEnd(() -> setShooterVoltage(voltage), () -> stopMotors());
+  public Command runShooter(DoubleSupplier velocity) {
+    return run(() -> updateGoal(velocity.getAsDouble())).finallyDo(() -> setShooterVoltage(0));
   }
 
-  public Command runVoltageRight(double voltage) {
-    return this.runEnd(() -> setVoltageRight(voltage), () -> stopMotors());
+  public Command runShooter(double velocity) {
+    return runShooter(() -> velocity);
   }
 }
