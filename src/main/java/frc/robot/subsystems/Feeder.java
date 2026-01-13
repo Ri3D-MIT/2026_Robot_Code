548826160261535f255b59df8a58fb0b0aa8e7c1
sigalrmp <sigalrmp@gmail.com>
2026-01-13 11:50:52 -0500

    _

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -6,42 +6,42 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.FaultLogger;
 
-public class Feeder extends SubsystemBase{
-    private final TalonFX feederMotor;
-    private final TalonFXConfiguration feederConfiguration;
+public class Feeder extends SubsystemBase {
+  private final TalonFX feederMotor;
+  private final TalonFXConfiguration feederConfiguration;
 
-    private int kFeederCurrentLimit = 15;
+  private int kFeederCurrentLimit = 15;
 
-    public Feeder() {
-        feederMotor = new TalonFX(FEEDER);
-        feederConfiguration = new TalonFXConfiguration();
-        
-        feederConfiguration.CurrentLimits.SupplyCurrentLimit = kFeederCurrentLimit;
-        feederConfiguration.CurrentLimits.SupplyCurrentLimitEnable = true;
+  public Feeder() {
+    feederMotor = new TalonFX(FEEDER);
+    feederConfiguration = new TalonFXConfiguration();
 
-        feederConfiguration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-        feederConfiguration.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+    feederConfiguration.CurrentLimits.SupplyCurrentLimit = kFeederCurrentLimit;
+    feederConfiguration.CurrentLimits.SupplyCurrentLimitEnable = true;
 
-        feederMotor.getConfigurator().apply(feederConfiguration);
+    feederConfiguration.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    feederConfiguration.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
 
-        FaultLogger.register(feederMotor);
-    }
+    feederMotor.getConfigurator().apply(feederConfiguration);
 
-    public void stopFeeder() {
-        feederMotor.stopMotor();
-    }
+    FaultLogger.register(feederMotor);
 
-    public void setVoltageFeeder(double voltage) {
-        feederMotor.setVoltage(voltage);
-    }
+    setDefaultCommand(runOnce(this::stopFeeder).andThen(idle()));
+  }
 
-    public Command runFeeder(double voltage) {
-        return this.runEnd(() -> setVoltageFeeder(voltage), () -> stopFeeder());
-    }
+  public void stopFeeder() {
+    feederMotor.stopMotor();
+  }
 
+  public void setVoltageFeeder(double voltage) {
+    feederMotor.setVoltage(voltage);
+  }
+
+  public Command runFeeder(double voltage) {
+    return this.runEnd(() -> setVoltageFeeder(voltage), () -> stopFeeder());
+  }
 }
