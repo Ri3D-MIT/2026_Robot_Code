d5c32ea1c53b6b18129cb875bcc6dba8cc3b9a41
Ryan Ruiz <ruiz.ryan.nj@gmail.com>
2026-01-12 19:23:35 -0500

    Removed test code, located ports to Ports and added PID/Feedforward to intake

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,7 +1,18 @@
 package frc.robot;
 
 import java.util.Map;
+import static java.util.Map.entry;
+
 
 public final class Ports {
-  public static final Map<Integer, String> idToName = Map.ofEntries();
+  public static final Map<Integer, String> idToName = 
+    Map.ofEntries(
+      entry(Intake.INTAKE_MOTOR, "intake motor"),
+      entry(Intake.PIVOT_MOTOR, "pivot motor"));
+
+  public static final class Intake {
+      public static final int INTAKE_MOTOR = 00;
+      public static final int PIVOT_MOTOR = 01;
+  }
+  
 }
