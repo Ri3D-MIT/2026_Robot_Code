51bcd1294867d4423807ca6b1ca764d20e7278dd
sigalrmp <sigalrmp@gmail.com>
2026-01-13 09:14:09 -0500

    _

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,78 +4,37 @@
 
 package frc.robot;
 
+import static edu.wpi.first.wpilibj2.command.button.RobotModeTriggers.*;
+
 import com.ctre.phoenix6.HootAutoReplay;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.epilogue.Epilogue;
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.lib.CommandRobot;
+import frc.lib.FaultLogger;
 
-public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
-
-  private final RobotContainer m_robotContainer;
-
+@Logged
+public class Robot extends CommandRobot {
   /* log and replay timestamp and joystick data */
   private final HootAutoReplay m_timeAndJoystickReplay =
       new HootAutoReplay().withTimestampReplay().withJoystickReplay();
 
   public Robot() {
-    m_robotContainer = new RobotContainer();
+    super(0.02);
+    configureGameBehavior();
+    configureBindings();
   }
+
+  public void configureGameBehavior() {
+    addPeriodic(FaultLogger::update, 2);
+    Epilogue.bind(this);
+  }
+
+  public void configureBindings() {}
 
   @Override
   public void robotPeriodic() {
     m_timeAndJoystickReplay.update();
     CommandScheduler.getInstance().run();
   }
-
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void disabledExit() {}
-
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      CommandScheduler.getInstance().schedule(m_autonomousCommand);
-    }
-  }
-
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void autonomousExit() {}
-
-  @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      CommandScheduler.getInstance().cancel(m_autonomousCommand);
-    }
-  }
-
-  @Override
-  public void teleopPeriodic() {}
-
-  @Override
-  public void teleopExit() {}
-
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
-
-  @Override
-  public void testPeriodic() {}
-
-  @Override
-  public void testExit() {}
-
-  @Override
-  public void simulationPeriodic() {}
 }
